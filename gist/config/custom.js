387fbd2d05b8ccb4f99260de23d615a2672045a5
pgray <p@pgray.org>
2022-04-06 17:11:13 -0400

    stand up sailsjs

---
+++ gist/config/custom.js
@@ -0,0 +1,120 @@
+/**
+ * Custom configuration
+ * (sails.config.custom)
+ *
+ * One-off settings specific to your application.
+ *
+ * For more information on custom configuration, visit:
+ * https://sailsjs.com/config/custom
+ */
+
+module.exports.custom = {
+
+  /**************************************************************************
+  *                                                                         *
+  * The base URL to use during development.                                 *
+  *                                                                         *
+  * • No trailing slash at the end                                          *
+  * • `http://` or `https://` at the beginning.                             *
+  *                                                                         *
+  * > This is for use in custom logic that builds URLs.                     *
+  * > It is particularly handy for building dynamic links in emails,        *
+  * > but it can also be used for user-uploaded images, webhooks, etc.      *
+  *                                                                         *
+  **************************************************************************/
+  baseUrl: 'http://localhost:1337',
+
+  /**************************************************************************
+  *                                                                         *
+  * Display dates for your app                                              *
+  *                                                                         *
+  * > This is here to make it easier to change out the copyright date       *
+  * > that is displayed all over the app when it's first generated.         *
+  *                                                                         *
+  **************************************************************************/
+  platformCopyrightYear: '2022',
+
+  /**************************************************************************
+  *                                                                         *
+  * The TTL (time-to-live) for various sorts of tokens before they expire.  *
+  *                                                                         *
+  **************************************************************************/
+  passwordResetTokenTTL: 24*60*60*1000,// 24 hours
+  emailProofTokenTTL:    24*60*60*1000,// 24 hours
+
+  /**************************************************************************
+  *                                                                         *
+  * The extended length that browsers should retain the session cookie      *
+  * if "Remember Me" was checked while logging in.                          *
+  *                                                                         *
+  **************************************************************************/
+  rememberMeCookieMaxAge: 30*24*60*60*1000, // 30 days
+
+  /**************************************************************************
+  *                                                                         *
+  * Automated email configuration                                           *
+  *                                                                         *
+  * Sandbox Sendgrid credentials for use during development, as well as any *
+  * other default settings related to "how" and "where" automated emails    *
+  * are sent.                                                               *
+  *                                                                         *
+  * (https://app.sendgrid.com/settings/api_keys)                            *
+  *                                                                         *
+  **************************************************************************/
+  // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
+  //--------------------------------------------------------------------------
+  // /\  Configure this to enable support for automated emails.
+  // ||  (Important for password recovery, verification, contact form, etc.)
+  //--------------------------------------------------------------------------
+
+  // The sender that all outgoing emails will appear to come from.
+  fromEmailAddress: 'noreply@gist.gg',
+  fromName: 'The Gist Team',
+
+  // Email address for receiving support messages & other correspondences.
+  // > If you're using the default privacy policy, this will be referenced
+  // > as the contact email of your "data protection officer" for the purpose
+  // > of compliance with regulations such as GDPR.
+  internalEmailAddress: 'support@gist.gg',
+
+  // Whether to require proof of email address ownership any time a new user
+  // signs up, or when an existing user attempts to change their email address.
+  verifyEmailAddresses: false,
+
+  /**************************************************************************
+  *                                                                         *
+  * Billing & payments configuration                                        *
+  *                                                                         *
+  * (https://dashboard.stripe.com/account/apikeys)                          *
+  *                                                                         *
+  **************************************************************************/
+  // stripePublishableKey: 'pk_test_Zzd814nldl91104qor5911gjald',
+  // stripeSecret: 'sk_test_Zzd814nldl91104qor5911gjald',
+  //--------------------------------------------------------------------------
+  // /\  Configure these to enable support for billing features.
+  // ||  (Or if you don't need billing, feel free to remove them.)
+  //--------------------------------------------------------------------------
+
+  /***************************************************************************
+  *                                                                          *
+  * Any other custom config this Sails app should use during development.    *
+  *                                                                          *
+  ***************************************************************************/
+  // …
+
+  // Storage settings
+  s3Region: 'nyc3',
+  s3Endpoint: 'nyc3.digitaloceanspaces.com',
+  staticContentS3Bucket: 'gist-static',
+  staticContentS3EdgeUrl: 'https://gist-static.nyc3.cdn.digitaloceanspaces.com',
+  userContentS3Bucket: 'gist-dev-cdn',
+  userContentS3EdgeUrl: 'https://gist-dev-cdn.nyc3.cdn.digitaloceanspaces.com',
+  // File size limits
+  userMaxImageSizeBytes: 5 * 1024 * 1024,
+  userMaxImageSizeFriendly: '5 MB',
+  userMaxMediaSizeBytes: 500 * 1024 * 1024,
+  userMaxMediaSizeFriendly: '500 MB',
+  // Pagination
+  userListPostsPerPage: 10
+
+};
