896df15987f68170ae2b912c950fca14eada3d85
pgray <p@pgray.org>
2022-04-10 18:53:09 -0400

    schema for a post

---
+++ gist/api/models/Post.js
@@ -0,0 +1,50 @@
+/**
+ * Post.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    textContent: {
+      type: 'string',
+      maxLength: 2000,
+      description: 'Text content if this is a text post'
+    },
+    imageContent: {
+      type: 'string',
+      description: 's3 object path if this is an image post'
+    },
+    contentType: {
+      type: 'string',
+      isIn: ['text', 'image', 'reblog']
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    user: {
+      model: 'user',
+      allowNull: false,
+      autoMigrations: { index: true }
+    },
+    rebloggedPost: {
+      model: 'post',
+      autoMigrations: { index: true },
+      description: 'The post that got reblogged if this is a reblog post'
+    }
+  },
+
+};
+
