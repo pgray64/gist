c633a8f73462268c1833ab0c5deac3ab2035e4b5
pgray <p@pgray.org>
2022-05-10 16:08:09 -0400

    legal stuff

--- gist/config/routes.js
+++ gist/config/routes.js
@@ -16,7 +16,7 @@ module.exports.routes = {
   'GET /':                   { action: 'view-homepage-or-redirect' },
   'GET /welcome/:unused?':   { action: 'dashboard/view-welcome' },
 
-  'GET /faq':                { action:   'view-faq' },
+  /*'GET /faq':                { action:   'view-faq' },*/
   'GET /legal/terms':        { action:   'legal/view-terms' },
   'GET /legal/privacy':      { action:   'legal/view-privacy' },
   /*'GET /contact':            { action:   'view-contact' },*/
